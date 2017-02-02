117dd5f7ac61352148d9cef0c794140cb38f065a
caleb <caleb.ray.williams@gmail.com>
2017-02-01 19:25:35 -0600

    InterfaceTestSolution
    Adding interface solution for development area of interface objects

---
+++ InterfaceTests/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    public interface StreamAPI
+    {
+        Task<bool> IsConnected(); 
+        Task<JsonResult> 
+    }
+}
